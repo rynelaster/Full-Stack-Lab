97ee2ad78b8c11ee1aa7933f98ea681c21ea6e2a
Ryne Laster <ryne.laster@gmail.com>
2018-01-12 12:22:52 -0600

    Created post route for new tacos

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const mongoose = require('mongoose');
 
+const bodyParser = require('body-parser')
+
+const methodOverride = require('method-override')
+
 
 // DB Stuff
 mongoose.connect('mongodb://localhost:27017/tacos', {useMongoClient: true })
@@ -12,12 +16,24 @@ mongoose.connection.once('open', (req, res)=>{
 	console.log('Connected to Mongo')
 })
 
+// Middleware
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({extended: false}));
+
 // This is our NEW Route
 app.get('/tacos/new', (req, res)=>{
 	res.render('new.ejs', )
 })
 
-
+// Post Route
+app.post('/tacos/', (req, res) =>{
+	if (req.body.isSpicy === 'on') {
+		req.body.isSpicy = true
+	} else {
+		req.body.isSpicy = false
+	}
+	res.send(req.body)
+})
 
 
 
