0e5d6d95b8cad342889ca33416a43fd8e58c657a
Ryne Laster <ryne.laster@gmail.com>
2018-01-12 12:10:18 -0600

    connected to mongo

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
 
 
+// DB Stuff
+mongoose.connect('mongodb://localhost:27017/tacos', {useMongoClient: true })
+// Check said DB
+mongoose.connection.once('open', (req, res)=>{
+	console.log('Connected to Mongo')
+})
+
+// This is our NEW Route
+app.get('/tacos/new', (req, res)=>{
+	res.render('new.ejs', )
+})
 
 
 
