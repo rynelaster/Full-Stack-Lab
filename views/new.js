85796b9f2657c6f08fb9977d630072aa4c4e5ccf
Ryne Laster <ryne.laster@gmail.com>
2018-01-12 12:00:44 -0600

    Created 'new' form for user to create tacos

---
+++ views/new.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>New Taco</title>
+</head>
+<body>
+
+	<h1>Add a Taco!</h1>
+	<form action="/tacos" method="POST">
+		Name: <input type="text" name="name">
+		<br />
+		Type: <input type="text" name="type">
+		<br />
+		Is spicy: <input type="checkbox" name="isSpicy">
+		<br />
+		
+			<input type="submit" name="" value="Create Taco">
+
+		</form>
+
+
+</body>
+</html>
